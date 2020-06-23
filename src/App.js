b0d3d6cb78dccd3b3450a8c7304fbf2c1585f760
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-06-24 04:19:39 +0530

    added a dummy file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Header/>
+      <Balance/>
+      <IncomeExpense/>
+      <TransactionList/>
+      <AddTransaction/>
     </div>
   );
 }
---
+++ src/components/IncomeExpense.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function IncomeExpense() {
+    return (
+        <div>
+             <div className="inc-exp-container">
+                <div>
+                    <h4>Income</h4>
+                    <p id="money-plus" className="money plus">+$0.00</p>
+                </div>
+                <div>
+                    <h4>Expense</h4>
+                    <p id="money-minus" className="money minus">-$0.00</p>
+                </div>
+            </div>
+        </div>
+    )
+}
