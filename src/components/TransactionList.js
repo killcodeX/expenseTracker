bbbc4233e4fd954fd43bce9139f8de49021d08b3
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-06-24 05:56:25 +0530

    TransactionList ui done

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 export default function TransactionList() {
     return (
-        <div className='box'>
-            <h3>History</h3>
-            <ul className="list">
-                <li className="minus">
-                    Cash <span>-$400</span><button className="delete-btn">x</button>
-                </li>
-            </ul>
-        </div>
+        <>
+            <h3 className='title is-4 has-text-centered'>History</h3>
+            <div className='box' id='transactionList'>
+                <ul className="list">
+                    <li className="minus">
+                        Cash <span className='title is-5'>-$400</span><button className="delete">x</button>
+                    </li>
+                </ul>
+            </div>
+        </>
     )
 }
